8aa50e63612b02d734ca7c39ca3e8a8013968b0a
mattdusud <mattdusud@gmail.com>
2026-09-04 14:23:17 +0200

    modifs tailles texte et classes

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,8 +4,8 @@ export default function Contact() {
 
     return (
         <>
-       <p><span>Email: </span><a href="mailto: mattdusud@gmail.com">matt@multimatt.fr</a></p>
-       <p><span>LinkedIn: </span><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/matthieu-giardina-727259b4/">Matthieu Giardina</a></p>
+       <p className='contact-text'><span>Email: </span><a href="mailto: mattdusud@gmail.com">matt@multimatt.fr</a></p>
+       <p className='contact-text'><span>LinkedIn: </span><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/matthieu-giardina-727259b4/">Matthieu Giardina</a></p>
         </>
     )
 }
